854f7984160aced366c045a30e898e35f03cf9c0
Gowri-TM <68550987+Gowri-TM@users.noreply.github.com>
2020-07-20 19:38:01 +0530

    Assignment 1 .py

---
+++ Assignment 1.py
@@ -0,0 +1,43 @@
+Name : Gowri TM
+Email-id : gowrimanjunath02@gmail.com
+
+Batch 6
+Day 1 :
+Assignment 1
+
+1. What is Python? What are the ways to install python in your PC?
+Python is a general-purpose, high-level, interpreted language created by Guido
+Van Rossum in 1991. It is an open source language. Python is a dynamically
+typed which supports multiple programming paradigms including structured
+and object-oriented programming.
+
+Ways to install python :
+A. Go to python official website
+www.python.org
+Download the latest version 3.8.4
+
+B. Go to anaconda official website
+www.anaconda.com
+Select individual edition from the product dropdown menu and download
+the latest version by choosing your appropriate OS
+
+C. Install Pycharm by visiting www.jetbrains.com/Pycharm.
+
+D. Use Google Colab where you can run the codes without downloading.
+
+2. What are Code Environments ? Types?
+A Code Environments is a standalone and self-contained environment to run
+Python code. Each code environments has its own set of packages. It is a
+environment where you can write , complie , run the programs and can see the
+expected output.
+
+There are 3 types of Code Environment
+A. Text editors : It is used for any text file.
+Eg- Notepad , Visual studio code
+
+B. Full IDE : Developed for large programs specifically for Python.
+Eg - Pycharm , Spyder
+
+C. Notebook Environments : Here we can see both input and output next to
+each other and it is great for learning.
+Eg - Jupyter Notebook
